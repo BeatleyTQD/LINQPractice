0c2c76ff14f4b58c26c7d2c4fb0645d8a77c2cf2
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-08-27 15:57:24 -0500

    Started third practice

--- Program.cs
+++ Program.cs
@@ -15,11 +15,67 @@ namespace linq
             where fruit.StartsWith("L")
             select fruit;
 
+            Console.WriteLine("----Practice 1----");
             foreach (string LFruit in LFruits)
             {
                 Console.WriteLine(LFruit);
-
             }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(n => n % 4 == 0 || n % 6 == 0);
+
+            Console.WriteLine("----Practice 2----");
+            foreach (int number in fourSixMultiples)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather",
+                "James",
+                "Xavier",
+                "Michelle",
+                "Brian",
+                "Nina",
+                "Kathleen",
+                "Sophia",
+                "Amir",
+                "Douglas",
+                "Zarley",
+                "Beatrice",
+                "Theodora",
+                "William",
+                "Svetlana",
+                "Charisse",
+                "Yolanda",
+                "Gregorio",
+                "Jean-Paul",
+                "Evangelina",
+                "Viktor",
+                "Jacqueline",
+                "Francisco",
+                "Tre"
+            };
         }
     }
 }
