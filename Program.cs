e776d24f8a71e35532d9a20585b2a101e35c289e
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-08-28 09:35:17 -0500

    Finished practice 8

--- Program.cs
+++ Program.cs
@@ -211,6 +211,44 @@ namespace linq
 
                 Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
             */
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Build a collection of customers who are millionaires
+
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer() { Name = "Bob Lesman", Balance = 80345.66, Bank = "FTB" },
+                new Customer() { Name = "Joe Landy", Balance = 9284756.21, Bank = "WF" },
+                new Customer() { Name = "Meg Ford", Balance = 487233.01, Bank = "BOA" },
+                new Customer() { Name = "Peg Vale", Balance = 7001449.92, Bank = "BOA" },
+                new Customer() { Name = "Mike Johnson", Balance = 790872.12, Bank = "WF" },
+                new Customer() { Name = "Les Paul", Balance = 8374892.54, Bank = "WF" },
+                new Customer() { Name = "Sid Crosby", Balance = 957436.39, Bank = "FTB" },
+                new Customer() { Name = "Sarah Ng", Balance = 56562389.85, Bank = "FTB" },
+                new Customer() { Name = "Tina Fey", Balance = 1000000.00, Bank = "CITI" },
+                new Customer() { Name = "Sid Brown", Balance = 49582.68, Bank = "CITI" }
+            };
+            List<MilliCustomersReport> customerReport = (from customer in customers group customer by customer.Bank into milliGroup select new MilliCustomersReport
+            {
+                BankName = milliGroup.Key,
+                    CustomerCount = milliGroup.Count()
+            }).ToList();
+
+            Console.WriteLine("----Exercise 8-----");
+            customerReport.ForEach(report => Console.WriteLine($"{report.BankName}: {report.CustomerCount}"));
+
+        }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
+        public class MilliCustomersReport
+        {
+            public string BankName { get; set; }
+            public int CustomerCount { get; set; }
         }
     }
 }
